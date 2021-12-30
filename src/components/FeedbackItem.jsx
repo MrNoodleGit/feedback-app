9ba0f8ea55d4a859f2b434c37a0f2705c6a9c98b
Ra Mour <mrnoodle@Ras-MacBook-Pro.local>
2021-12-30 15:56:16 -0500

    first commit; state & useState Hook

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+
+function FeedbackItem() {
+  const [] = useState()
+  
+  return (
+    <div className='card'>
+      <div className="num-display">10</div>
+      <div className="text-display">
+        This is an example of a feedback item.
+      </div>
+    </div>
+  )
+}
+
+export default FeedbackItem
